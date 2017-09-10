bf69832882ac26841c1a0739d723a6a9848cedec
Junnan Tang <junnan.tang.2014@gmail.com>
2017-09-10 16:35:32 -0700

    add student class

--- index.php
+++ index.php
@@ -11,8 +11,43 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
-        echo "Junnan Tang";
+        include('Student.php');
+
+        $students = array();
+
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home', 'john@doe.com');
+        $first->add_email('work', 'jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
+
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home', 'albert@braniacs.com');
+        $second->add_email('work1', 'a_einstein@bcit.ca');
+        $second->add_email('work2', 'albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
+        
+        $third = new Student();
+        $third->surname = "Tang";
+        $third->first_name = "Junnan";
+        $third->add_email('work', 'junnan.tang.2014@gmail.com');
+        $third->add_grade(80);
+        $third->add_grade(90);
+        $third->add_grade(70);
+        $students['a000'] = $third;
+        
+        ksort($students);
+        foreach($students as $student)
+            echo $student->toString();
         ?>
     </body>
 </html>
